fe067612ba99bb774bd46f6cbeaddced2bf4b66d
zclut <juandanielpadilla0@gmail.com>
2024-07-24 17:42:53 +0100

    add const file

---
+++ src/utils/const.ts
@@ -0,0 +1,9 @@
+const GITHUB_URL: string = "https://github.com"
+
+const IMAGE_AUTHORS_PATH: string = "/images/authors"
+
+export const GITHUB_ACCOUNTS: { name: string, url: string, image: string }[] = [
+    { name: "zClut", url: `${GITHUB_URL}/zclut`, image: `${IMAGE_AUTHORS_PATH}/zclut.webp` },
+    { name: "klasinky", url: `${GITHUB_URL}/klasinky`, image: `${IMAGE_AUTHORS_PATH}/klasinky.webp` },
+    { name: "romentoss", url: `${GITHUB_URL}/romentoss`, image: `${IMAGE_AUTHORS_PATH}/romentoss.webp` }
+]
