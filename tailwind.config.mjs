89959aa4d871f04c4a19ba6a940fb71b97e36ff1
zclut <juandanielpadilla0@gmail.com>
2024-07-24 17:35:29 +0100

    add colors to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#1DB954",
+				secondary: "#121212",
+				tertiary: "#212121"
+			}
+		},
 	},
 	plugins: [],
 }
